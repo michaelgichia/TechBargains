6a5b431e0aaf7a77213a539340855d7b33d09655
mike_gichia <mqyynm@gmail.com>
2017-06-20 12:43:51 +0300

    Clean up.

--- app/app.js
+++ app/app.js
@@ -27,7 +27,7 @@ import LanguageProvider from 'containers/LanguageProvider';
 
 // Load the favicon, the manifest.json file and the .htaccess file
 /* eslint-disable import/no-unresolved, import/extensions */
-import '!file-loader?name=[name].[ext]!./logo_Sel_icon.ico';
+import '!file-loader?name=[name].[ext]!./favicon.ico';
 import '!file-loader?name=[name].[ext]!./manifest.json';
 // import 'file-loader?name=[name].[ext]!./.htaccess';
 // Bootstraps imports
