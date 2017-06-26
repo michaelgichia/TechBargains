68296bc9836c0018af28da17fc7b05389f872207
mike_gichia <mqyynm@gmail.com>
2017-06-26 06:52:57 +0300

    404 page.

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -9,17 +9,48 @@
  * the linting exception.
  */
 
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import React from "react";
+import RaisedButton from "material-ui/RaisedButton";
 
-import messages from './messages';
+import "!!style-loader!css-loader!./style.css";
 
-export default class NotFound extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+export default class NotFound extends React.PureComponent {
+  // eslint-disable-line react/prefer-stateless-function
+
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div className="body">
+        <div className="cont_principal cont_error_active">
+          <div className="cont_error">
+
+            <h1>404</h1>
+            <p>The Page you're looking for isn't here.</p>
+            <RaisedButton
+              backgroundColor="#525c65"
+              href="https://deals-expert.com"
+              labelStyle={{
+                textTransform: "none",
+                paddingLeft: 50,
+                paddingRight: 50,
+                fontSize: 20,
+                fontWeight: 700
+              }}
+              labelColor="#fff"
+              label="Home"
+              style={styles.button}
+            />
+          </div>
+          <div className="cont_aura_1" />
+          <div className="cont_aura_2" />
+        </div>
+
+      </div>
     );
   }
 }
+
+const styles = {
+  button: {
+    margin: 12
+  },
+};
