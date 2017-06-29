5252c9b64dc94afd7ecda535eca0a57c7ea09d54
mike_gichia <mqyynm@gmail.com>
2017-06-29 13:57:22 +0300

    Storedata.

---
+++ app/containers/NavBar/storeData.js
@@ -0,0 +1,19 @@
+const Stores = [
+  { title: "Amazon" },
+  { title: "AT&T Wireless" },
+  { title: "B&H Photo Video" },
+  { title: "Dell" },
+  { title: "Dell Small Business" },
+  { title: "eBay" },
+  { title: "Groupon" },
+  { title: "HP" },
+  { title: "Lenovo" },
+  { title: "Microsoft Store" },
+  { title: "Office Depot® & OfficeMax®" },
+  { title: "Sprint" },
+  { title: "T-Mobile" },
+  { title: "Verizon Wireless" },
+  { title: "cdkeys" },
+  { title: "DailySteals" },
+  { title: "TorGuard" }
+];
