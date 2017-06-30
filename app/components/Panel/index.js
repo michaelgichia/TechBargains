bb6f76fa8bba134ae5acbec261d0eda5f89b9d54
mike_gichia <mqyynm@gmail.com>
2017-06-30 14:13:40 +0300

    Panel Component.

--- app/components/Panel/index.js
+++ app/components/Panel/index.js
@@ -4,29 +4,22 @@
 *
 */
 
-import React from 'react';
+import React from "react";
 import "!!style-loader!css-loader!./style.css";
-
 
 function Panel({ headerElement, children }) {
   return (
-    <div style={{padding: 30}}>
     <div className="react-panel">
-    <div className="react-panel-header">
-      {headerElement}
-    </div>
-    <div className="react-panel-body">
-      {children}
-    </div>
-
-    </div>
-
+      <div className="react-panel-header">
+        {headerElement}
+      </div>
+      <div className="react-panel-body">
+        {children}
+      </div>
     </div>
   );
 }
 
-Panel.propTypes = {
-
-};
+Panel.propTypes = {};
 
 export default Panel;
