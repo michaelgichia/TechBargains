ecc776f0a9fe6a29966b7c262f44ff479c619c4d
mike_gichia <mqyynm@gmail.com>
2017-07-01 05:06:36 +0300

    UI for 480px.

--- app/components/ProductDetail/index.js
+++ app/components/ProductDetail/index.js
@@ -2,143 +2,45 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router";
 import YesNoBtn from "components/YesNoBtn";
-import Row from "react-bootstrap/lib/Row";
-import Grid from "react-bootstrap/lib/Grid";
-import Col from "react-bootstrap/lib/Col";
-import { Image, Transformation } from "cloudinary-react";
-import Panel from "react-bootstrap/lib/Panel";
+import { Image } from "cloudinary-react";
+// import Panel from "react-bootstrap/lib/Panel";
+// Our Components
+import Panel from "components/Panel";
+
+import { timeConversion } from "utils/milliSecondsConverter";
 import "!!style-loader!css-loader!./style.css";
 
-class ProductDetail extends React.Component {
-  // eslint-disable-line react/prefer-stateless-function
+//Try
+import { Row, Col } from "reactstrap";
 
-  timeConversion = expireAt => {
-    const expire = new Date(expireAt).getTime();
-    const millisec = expire - new Date().getTime();
-    const seconds = (millisec / 1000).toFixed(1);
-    const minutes = (millisec / (1000 * 60)).toFixed(1);
-    const hours = (millisec / (1000 * 60 * 60)).toFixed(1);
-    const days = (millisec / (1000 * 60 * 60 * 24)).toFixed(1);
-
-    if (seconds < 60) {
-      return seconds + " SEC";
-    } else if (minutes < 60) {
-      return minutes + " MIN";
-    } else if (hours < 24) {
-      return hours + " HRS";
-    } else {
-      return days + " DAYS";
-    }
-  };
-
+class ProductDetail extends React.PureComponent {
   render() {
+    const { product, onTouchTap } = this.props;
     return (
-      <Panel
-        className="merchant-panel"
-        header={
-          <div className="toppers">
-            {`EXPIRE: ${this.timeConversion(this.props.product.expire)}`}
-          </div>
-        }
-      >
-        <div className="merchant-panel-header">
-          <Link to={`/product/${this.props.product.id}`}>
-            {this.props.product.name}
-          </Link>
-        </div>
-        <Grid fluid>
-
+      <Panel headerElement={<PanelHeader product={product} />}>
+        <div className="custom-row">
           <Row>
-
-            <div className="product-detail-image">
-              <Col xs={4} sm={4} md={4} lg={4}>
-                <div className="product-detail-image-image">
-                  <a href={this.props.product.backlink} target="_blank">
-                    <Image publicId={this.props.product.public_id}>
-                        <Transformation
-                          width="200"
-                          crop="scale"
-                          height="200"
-                          dpr="auto"
-                        />
-                      </Image>
-                   </a>
-                </div>
-              </Col>
-            </div>
-            <Col
-              className="product-detail-features"
-              xs={8}
-              sm={8}
-              md={8}
-              lg={8}
-            >
-              <div>
-                <div className="line-clamp-wrapper">
-                  <div
-                    className="line-clamp"
-                    dangerouslySetInnerHTML={{
-                      __html: this.props.product.features
-                    }}
-                  />
-                </div>
-                <div className="price">
-                  <div className="more-details-wrapper">
-                    <Link
-                      to={`/product/${this.props.product.id}`}
-                      className="more-details"
-                    >
-                      More Details
-                    </Link>
-                  </div>
-                  <div className="price-wrapper">
-                    <p>
-                      <span className="price-first-span">
-                        {this.props.product.percentage}
-                      </span>
-                      <span className="price-second-span">
-                        {this.props.product.isShipped}
-                      </span>
-                    </p>
-                  </div>
-                  <YesNoBtn
-                    isCoupon={this.props.product.isCoupon}
-                    onTouchTap={this.props.onTouchTap}
-                    backlink={this.props.product.backlink}
-                  />
-                  <div className="bottom-wrapper">
-                    <p>
-                      From{" "}
-                      <Link
-                        style={{
-                          color: "#b0b0b0",
-                          textDecoration: "underline"
-                        }}
-                      >
-                        {this.props.product.merchant !== null 
-                          ? this.props.product.merchant.title
-                          :""}
-                      </Link>{" "}
-                      in{" "}
-                      <Link
-                        style={{
-                          color: "#b0b0b0",
-                          textDecoration: "underline"
-                        }}
-                      >
-                        {this.props.product.category.name}
-                      </Link>
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-            </Col>
-
+            <PanelHeaderTitle product={product} />
           </Row>
-
-        </Grid>
-
+        </div>
+        <div>
+          <Row className="custom-body-row">
+            <CustomCol xxs="12" xs="12" sm="4" md="4" lg="4" xl="4" xxl="2">
+              <PanelImage product={product} />
+            </CustomCol>
+            <CustomCol xxs="12" xs="12" sm="8" md="8" lg="8" xl="8" xxl="4">
+              <PanelFeatures product={product} />
+              <PanelReadMore product={product} />
+              <PanelPriceWrapper product={product} />
+              <YesNoBtn
+                isCoupon={product.isCoupon}
+                onTouchTap={onTouchTap}
+                backlink={product.backlink}
+              />
+              <PanelBottomInfo product={product} />
+            </CustomCol>
+          </Row>
+        </div>
       </Panel>
     );
   }
@@ -152,3 +54,79 @@ ProductDetail.propTypes = {
 ProductDetail.defaultProps = {};
 
 export default ProductDetail;
+
+const PanelHeader = ({ product }) =>
+  <div>
+    {`EXPIRE: ${timeConversion(product.expire)}`}
+  </div>;
+
+const PanelHeaderTitle = ({ product }) =>
+  <h2 className="merchant-panel-header">
+    <Link to={`/product/${product.id}`}>
+      {product.name}
+    </Link>
+  </h2>;
+
+const PanelImage = ({ product }) =>
+  <div className="product-detail-image">
+    <a href={product.backlink} target="_blank">
+      <Image
+        cloudName="dw3arrxnf"
+        publicId={product.public_id}
+        crop="scale"
+        width="200"
+        responsive
+        alt={product.name}
+      />
+    </a>
+  </div>;
+
+const PanelBottomInfo = ({ product }) =>
+  <ul className="bottom-wrapper">
+    <li>
+      <span className="no-break">
+        From  {" "}
+        <a href={`/merchant/${product.merchant._id.toString()}`}>
+          {product.merchant !== null ? product.merchant.title : ""}
+        </a>
+      </span>
+      <span className="no-break">
+        {" "} in  {" "}
+        <a href={`/category/${product.category._id.toString()}`}>
+          {product.category.name}
+        </a>
+      </span>
+    </li>
+  </ul>;
+
+const PanelPriceWrapper = ({ product }) =>
+  <div className="price">
+    <p>
+      <span className="price-first-span">
+        {product.percentage}
+      </span>
+      <span className="price-second-span">
+        {product.isShipped}
+      </span>
+    </p>
+  </div>;
+
+const PanelReadMore = ({ product }) =>
+  <div className="more-details-wrapper">
+    <Link to={`/product/${product.id}`}>More Details</Link>
+  </div>;
+
+const PanelFeatures = ({ product }) =>
+  <div
+    className="panel-product-features"
+    dangerouslySetInnerHTML={{
+      __html: product.features
+    }}
+  />;
+
+const CustomCol = props =>
+  <Col
+    className="custom-column"
+    widths={["xxs", "xs", "sm", "md", "lg", "xl", "xxl"]}
+    {...props}
+  />;
