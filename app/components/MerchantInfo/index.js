086f262e38aa932906582e7930af36abe67eac6f
mike_gichia <mqyynm@gmail.com>
2017-07-03 06:17:03 +0300

    fixed responsiveness.

--- app/components/MerchantInfo/index.js
+++ app/components/MerchantInfo/index.js
@@ -17,12 +17,15 @@ import "!!style-loader!css-loader!./merchant-info.css";
 class MerchantInfo extends React.PureComponent {
   render() {
     const { info } = this.props;
+    console.log({ info });
     return (
       <Row>
-        <Col xs="12" sm="12" md="4" lg="3" xl="3">
-          <InfoImage info={info} />
-        </Col>
-        <Col xs="12" sm="12" md="8" lg="9" xl="9">
+        {Object.keys(info).length > 0 && info.public_id.length > 0
+          ? <Col xs="12" sm="12" md="12" lg="3" xl="3">
+              <InfoImage info={info} />
+            </Col>
+          : <div />}
+        <Col className="merchant-info-description-wrapper" xs="12" sm="12" md="12" lg="9" xl="9">
           <InfoTitle info={info} />
           <InfoDescription info={info} />
           {Object.keys(info).length > 0 ? <InfoBtn info={info} /> : <div />}
@@ -55,14 +58,14 @@ const InfoImage = ({ info }) =>
         crop="scale"
       />
     </a>
-  </div>
+  </div>;
 
 const InfoTitle = ({ info }) =>
   <div className="amazon-h1">
     {info.title !== undefined && info.title.length > 0
       ? <h1>{`${info.title} Deals & Coupons`}</h1>
-      : <div />}    
-  </div>
+      : <div />}
+  </div>;
 
 const InfoDescription = ({ info }) =>
   <div className="amazon-text">
